7f60f354a13ceb2b7975a5caef59b27ed1898c4c
Im SeongChang <pabi3564@gmail.com>
2018-05-01 19:07:02 +0900

    PythonChallenge Level11

---
+++ Level11/Level11.py
@@ -0,0 +1,22 @@
+#problem: http://www.pythonchallenge.com/pc/return/5808.html
+
+import requests
+from PIL import Image
+from io import BytesIO
+
+im = Image.open('cave.jpg')
+(w, h) = im.size
+
+even = Image.new('RGB', (w//2, h//2))
+odd = Image.new('RGB', (w//2, h//2))
+
+for i in range(w):
+    for j in range(h):
+        p = im.getpixel((i,j))
+        if (i+j)%2 == 1:
+            odd.putpixel((i//2, j//2), p)
+        else:
+            even.putpixel((i//2, j//2), p)
+
+even.save('even.jpg')
+odd.save('odd.jpg')
